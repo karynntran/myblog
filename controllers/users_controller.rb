f69107dfc4bcddbbb3425de462ac2bb789a63313
karynntran <karynn.tran@gmail.com>
2014-10-29 20:53:54 -0400

    Adding connections in to see if its causing an error

--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < Sinatra::Base
   helpers Sinatra::AuthenticationHelper
 
+  ActiveRecord::Base.establish_connection({
+    adapter: 'postgresql',
+    database: 'blog_db'
+  })
+
   set :views, File.expand_path('../../views',__FILE__)
 
   set :public_folder, File.expand_path('../../public',__FILE__)
