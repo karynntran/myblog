bcadb2ce34d32cb03e0f381f0f26f9c6336d77da
karynntran <karynn.tran@gmail.com>
2014-10-29 20:34:33 -0400

    moved connection to rakefile

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -2,11 +2,6 @@ class ApplicationController < Sinatra::Base
   helpers Sinatra::AuthenticationHelper
   helpers ActiveSupport::Inflector
 
-  ActiveRecord::Base.establish_connection({
-    adapter: 'postgresql',
-    database: 'blog_db'
-  })
-
   set :views, File.expand_path('../../views',__FILE__)
 
   set :public_folder, File.expand_path('../../public',__FILE__)
