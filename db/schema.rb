dd2f9652720a74b13a4ba121131e612baa30f091
karynntran <karynn.tran@gmail.com>
2014-11-01 13:49:15 -0400

    Update the tags table so that body is set to null:false
    This allows the tag edit feature to show up even if someone didn't initially add a tag to the blog post

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-head# encoding: UTF-8
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +11,7 @@ head# encoding: UTF-8
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141031032608) do
+ActiveRecord::Schema.define(version: 20141101173637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20141031032608) do
   end
 
   create_table "tags", force: true do |t|
-    t.text     "body"
+    t.text     "body",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
